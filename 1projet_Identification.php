e41f1409877b603b4b440d178f6e45e697553156
Duc khai VU <41749268+hwkhai@users.noreply.github.com>
2018-12-06 17:59:50 +0100

    Add files via upload

--- 1projet_Identification.php
+++ 1projet_Identification.php
@@ -1,48 +1,55 @@
-<?php
-session_start();
-@require_once("inc/autoload.inc.php");
-@include("inc/debutXHTML.inc.php");
-?>
-<?php echo "<script type='text/javascript'>identification();</script>";?>
-		<img class=image src="img/logo.png" alt="Logo"/>
-    <h1> Connexion </h1>
-		 <form method="post" action="identification.php">
-             <?php  include("inc/menu.inc.php")?>
-            <br/>
+<?php session_start();?>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Identifiants ATMO</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="1projet.css"/>
 
-             <fieldset >
-                 <legend><h4>INFORMATIONS PERSONNELLES</h4></legend><br />
-                <table>
-                    <tr>
-                        <td>
-                            <label for="">Email :</label>
-                        </td>
-                        <td>
-                            <input name="email" type="text"/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="">Mot de passe :</label>
-                        </td>
-                        <td>
-                            <input name="mdp" type="password"/><br>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2">
-                            <input type="submit" name="submit" value="OK"/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2">
-                            <a href=1projet_changemdp.php>Changer de mot de passe</a>
-                        </td>
-                    </tr>
-                </table>
-             </fieldset>
-             <br/>
-		</form>
+	</head>
+<body>
 <?php
-@include("inc/finXHTML.inc.php");
+if (!isset($_SESSION['nbessai']))
+	{ $_SESSION['nbessai']=1;
+	}
+echo "<p> Essai numéro : ",$_SESSION['nbessai'],"</p>";
+
 ?>
+		<img class=image src="logo.png"  alt="Logo"/>
+    <h1> Connexion </h1>
+		<form method="GET" action="1projet_test_identification.php">
+
+
+		<div class="scrollmenu">
+		  <a href=1projet_Acc.html>Home </a>
+		  <a href="#news">News</a>
+		  <a href="#contact">Contact</a>
+		  <a href="1About.html">About</a>
+      <a href="1projet_inscription.html">S'inscrire</a>
+			<a href="1projet_identification.html">Connexion</a>
+		</div>
+
+		<p> </p>
+    <br />
+
+
+			<fieldset><legend><h4>IDENTIFIANTS</h4></legend><br />
+						
+					<img class=image.left src="mail.png"  alt="Mail" hspace=10 />
+						<input name="id" type="text"/> <br /><br />
+				
+					<img class=image.left src="cle3.png"  alt="Mot de passe" hspace=10 />
+					<input name="mdp" type="password"/>  <br /><br />
+        	<input type="submit" name="OK" value="OK"/> <br /> <br />
+
+				<a href=1projet_changemdp.html>Changer de mot de passe</a>
+
+
+
+			</fieldset> <br />
+
+
+
+
+		</form>
+</body>
+</html>
